16e2843544ef8071880bf8e33c5f82ecde87f28a
shahjee362 <ahmedshah222666@gmail.com>
2023-10-01 15:54:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var username1 = "My Name is Ahmed"
+var username2 = "Shah"
+
+alert (username1 +" "+username2)
+
+var age = " I am 18 years old"
+
+alert(age)
+
+var education = "certified Mobile and web  development"
+alert(education)
+
+alert("Pizza \n Pizz \n Piz \n Pi \n  P \n")
+
+var email = "My Email Adress is ahmedshah222666@gmail.com"
+alert(email)
+
+var javascript = "i am trying to learnt from the book A master way to learn java script"
+alert(javascript)
+
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
